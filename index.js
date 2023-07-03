cced2a960f758300b383e1845475ef92cd3f8920
Story5 <337995545@qq.com>
2023-07-03 17:05:58 +0800

    打印

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+console.log('hello world');
+console.log(Date);
+console.log(Math);
+console.log(setTimeout);
+console.log(setInterval);
+
+// console.log(requestAnimationFrame);
+console.log(setImmediate);// 替代 requestAnimationFrame
+
+// node.js 特有
+console.log(__filename);
+console.log(__dirname);
+
+console.log(process);
