a25136e4a2bd07bd618b9eaed0c12a5ec37c59c5
Story5 <337995545@qq.com>
2023-07-07 11:55:44 +0800

    24 Buffer

---
+++ 02-3 Buffer/24 /index.js
@@ -0,0 +1,28 @@
+// Buffer 创建
+const buffer1 = Buffer.from('geekbang');
+const buffer2 = Buffer.from([1, 2, 3, 4]);
+
+const buffer3 = Buffer.alloc(20);
+
+console.log('buffer1:', buffer1);
+console.log('buffer2:', buffer2);
+console.log('buffer3:', buffer3);
+
+// Buffer 读写
+buffer2.writeInt8(12, 1);
+console.log('buffer2:', buffer2);
+
+buffer2.writeInt16BE(512, 2);
+console.log('buffer2:', buffer2);
+
+// 
+const fs = require('fs');
+const protobuf = require('protocol-buffers');
+const schema = protobuf(fs.readFileSync(__dirname + '/test.proto', 'utf-8'));
+console.log(schema);
+const buffer = schema.Column.encode({
+    id: 1,
+    name: 'Node.js',
+    price: 80.4
+}) 
+console.log(schema.Column.decode(buffer));
